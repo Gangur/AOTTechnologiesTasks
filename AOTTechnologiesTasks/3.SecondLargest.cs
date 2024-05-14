4a5fb629e44c695a385583358cabcf16b82b91cb
GangurDV <GangurDV@live.ru>
2024-05-14 16:43:15 +0200

    3. SecondLargest

---
+++ AOTTechnologiesTasks/3.SecondLargest.cs
@@ -0,0 +1,41 @@
+﻿namespace AOTTechnologiesTasks
+{
+    public static class SecondLargest
+    {
+        public static int Find(List<int> numbers) // better array
+        {
+            int n = numbers.Count;
+            if (n <= 1)
+                return -1;
+
+            int firstLargest = int.MinValue;
+            int secondLargest = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                firstLargest = Math.Max(firstLargest, numbers[i]);
+            }
+
+            bool firstFound = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] != firstLargest)
+                    secondLargest = Math.Max(secondLargest, numbers[i]);
+                else
+                {
+                    if (firstFound)
+                    {
+                        return numbers[i]; // For the case we have several max
+                    }
+                    else
+                    {
+                        firstFound = true;
+                    }
+                }
+
+            }
+
+            return secondLargest;
+        }
+    }
+}
--- AOTTechnologiesTasks/Program.cs
+++ AOTTechnologiesTasks/Program.cs
@@ -1,19 +1,11 @@
 ﻿using AOTTechnologiesTasks;
 
-public static class Program
+class Program
 {
-    //var summary = BenchmarkRunner.Run<Benchmark>();
-
     static void Main(string[] args)
     {
-        int n1 = 3;
-        string str1 = "abc de fghsij";
-        Console.WriteLine("Reshape(" + n1 + ", \"" + str1 + "\") = \"" + ReshapeWordsByNSymbols.Reshape(n1, str1) + "\"");
-
-        int n2 = 2;
-        string str2 = "1 23 456";
-        Console.WriteLine("Reshape(" + n2 + ", \"" + str2 + "\") = \"" + ReshapeWordsByNSymbols.Reshape(n2, str2) + "\"");
+        List<int> numbers = new List<int> { 5, 2, 9, 8, 9, 7 };
+        int result = SecondLargest.Find(numbers);
+        Console.WriteLine("Second Largest: " + result); // Output: Second Largest: 8
     }
 }
-
-
